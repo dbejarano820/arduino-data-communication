69ce8339266ca481b6f811a4c16d8d2d4c09196b
Blacksolid64 <48729275+Blacksolid64@users.noreply.github.com>
2023-10-26 15:11:19 -0600

    Bug correction regarding arduino sending and receiving

--- transmitter.ino
+++ transmitter.ino
@@ -4,23 +4,23 @@
 
 using namespace std;
 
-SoftwareSerial mySerial(10, 11); // RX, TX
+SoftwareSerial mySerial(10, 11);  // RX, TX
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-const uint8_t DEFAULT_FLAG = 0x7E; // 01111110 en binario
+const uint8_t DEFAULT_FLAG = 0x7E;  // 01111110 en binario
+const uint8_t ESCAPE_FLAG = 0xAA;   // Para byte stuffing
 const uint16_t speed = 300;
-const uint16_t payloadSize = 200;
+const uint16_t payloadSize = 400;
 static const size_t frameASize = 9;
 static const size_t frameBSize = 205;
 bool first_transmission = true;
 uint8_t data[payloadSize];
-enum : uint8_t
-{
+enum : uint8_t {
   initialHandshake = 0b000,
   infoFrame = 0b001,
   endCurrentComm = 0b010,
-  nonnitialHandshake = 0b011,
+  nonInitialHandshake = 0b011,
   finalComm = 0b100,
   ack = 0b101
 };
@@ -33,24 +33,18 @@ int numberOfTests = 1;
   Salidas: Devuelve el valor calculado sobre el CRC
 */
 
-uint16_t calculateCRC(uint8_t *data, size_t longitud)
-{
-  uint16_t crc = 0xFFFF; // Valor inicial del CRC
+uint16_t calculateCRC(uint8_t *data, size_t longitud) {
+  uint16_t crc = 0xFFFF;  // Valor inicial del CRC
 
-  for (size_t i = 0; i < longitud; ++i)
-  {
-    crc ^= data[i]; // Realiza la operación XOR con el byte actual
+  for (size_t i = 0; i < longitud; ++i) {
+    crc ^= data[i];  // Realiza la operación XOR con el byte actual
 
     // Realiza el cálculo para cada bit del byte
-    for (int j = 0; j < 8; ++j)
-    {
-      if (crc & 0x0001)
-      {
-        crc >>= 1;     // Bit shift
-        crc ^= 0xA001; // Polinomio generador para CRC-16, forma reversa de 0x8005 para adecuarse al XOR y otras operaciones de bits
-      }
-      else
-      {
+    for (int j = 0; j < 8; ++j) {
+      if (crc & 0x0001) {
+        crc >>= 1;      // Bit shift
+        crc ^= 0xA001;  // Polinomio generador para CRC-16, forma reversa de 0x8005 para adecuarse al XOR y otras operaciones de bits
+      } else {
         crc >>= 1;
       }
     }
@@ -59,88 +53,75 @@ uint16_t calculateCRC(uint8_t *data, size_t longitud)
   return crc;
 }
 
-uint8_t *buildFrameA(uint8_t type, uint16_t speed, uint16_t dataSize)
-{
-  static uint8_t frameA[9]; // Estático para preservar el valor entre llamadas
+uint8_t *buildFrameA(uint8_t type, uint16_t speed, uint16_t dataSize) {
+  static uint8_t frameA[9];  // Estático para preservar el valor entre llamadas
 
   frameA[0] = DEFAULT_FLAG;
   frameA[1] = type;
-  frameA[2] = speed >> 8;      // byte alto de velocidad
-  frameA[3] = speed & 0xFF;    // byte bajo de velocidad
-  frameA[4] = dataSize >> 8;   // byte alto de tamaño
-  frameA[5] = dataSize & 0xFF; // byte bajo de tamaño
+  frameA[2] = speed >> 8;       // byte alto de velocidad
+  frameA[3] = speed & 0xFF;     // byte bajo de velocidad
+  frameA[4] = dataSize >> 8;    // byte alto de tamaño
+  frameA[5] = dataSize & 0xFF;  // byte bajo de tamaño
 
-  uint16_t crc = calculateCRC(frameA + 1, 5); // Calcula el CRC de los datos en la trama (sin incluir banderas)
-  frameA[6] = crc >> 8;                       // byte alto de CRC
-  frameA[7] = crc & 0xFF;                     // byte bajo de CRC
+  uint16_t crc = calculateCRC(frameA + 1, 5);  // Calcula el CRC de los datos en la trama (sin incluir banderas)
+  frameA[6] = crc >> 8;                        // byte alto de CRC
+  frameA[7] = crc & 0xFF;                      // byte bajo de CRC
   frameA[8] = DEFAULT_FLAG;
 
   return frameA;
 }
 
-uint8_t *buildFrameB(uint8_t NS, uint8_t type, uint8_t *information)
-{
+uint8_t *buildFrameB(uint8_t NS, uint8_t type, uint8_t *information) {
   static uint8_t frameB[205];
 
   frameB[0] = DEFAULT_FLAG;
-  frameB[1] = (NS << 3) | type;         // Desplaza NS 3 bits a la izquierda y OR con tipo
-  memcpy(frameB + 2, information, 200); // Copia 200 bytes de información
+  frameB[1] = (NS << 3) | type;  // Desplaza NS 3 bits a la izquierda y OR con tipo
+  for (int i = 0 + 2; i < 200 + 2; i++) {
+    frameB[i] = information[i];
+  }
+  //memcpy(frameB + 2, information, 200);  // Copia 200 bytes de información
 
-  uint16_t crc = calculateCRC(frameB + 1, 202); // Calcula el CRC de los datos en la trama (sin incluir banderas)
-  frameB[202] = crc >> 8;                       // byte alto de CRC
-  frameB[203] = crc & 0xFF;                     // byte bajo de CRC
+  uint16_t crc = calculateCRC(frameB + 1, 202);  // Calcula el CRC de los datos en la trama (sin incluir banderas)
+  frameB[202] = crc >> 8;                        // byte alto de CRC
+  frameB[203] = crc & 0xFF;                      // byte bajo de CRC
   frameB[204] = DEFAULT_FLAG;
-
+  Serial.println("\n");
   return frameB;
 }
 
 // Missing acknowledge logic
-void sendFrame(uint8_t *frame, uint8_t NS, int frameType)
-{
+void sendFrame(uint8_t *frame, uint8_t NS, int frameType) {
   delay(5000);
-  if (frameType == -1)
-  {
+  if (frameType == -1) {
 
-    for (int i = 0; i < frameASize; i++)
-    {
-      Serial.write(*frame);
-      delay(50);
-      frame++;
+    for (int i = 0; i < frameASize; i++) {
+      Serial.write(frame[i]);
+      delay(100);
     }
-  }
-  else
-  {
-    for (int i = 0; i < frameBSize; i++)
-    {
-      Serial.write(*frame);
-      delay(50);
-      frame++;
+  } else if (frameType == 0) {
+    for (int i = 0; i < frameBSize; i++) {
+      Serial.write(frame[i]);
+      delay(100);
     }
   }
   // Serial.println("\nDone\n");
 }
 
-bool acked(uint8_t NS)
-{
+bool acked(uint8_t NS) {
   Serial.println("Sending...");
-  if (Serial.available())
-  {
+  if (Serial.available()) {
 
     uint8_t firstByte = Serial.read();
     uint8_t rawData = Serial.read();
     uint8_t lastByte = Serial.read();
-    if (firstByte == DEFAULT_FLAG && lastByte == DEFAULT_FLAG)
-    {
+    if (firstByte == DEFAULT_FLAG && lastByte == DEFAULT_FLAG) {
       uint8_t receivedSN = rawData | 0x07;
       uint8_t type = rawData & 0x07;
 
-      if ((NS != 0 || receivedSN == NS) && type == ack)
-      {
+      if ((NS != 0 || receivedSN == NS) && type == ack) {
         Serial.println("Acked...");
         return true;
-      }
-      else
-      {
+      } else {
         Serial.println("Not Acked...");
         return false;
       }
@@ -150,76 +131,111 @@ bool acked(uint8_t NS)
   }
 }
 
-void setup()
-{
+void setup() {
   lcd.begin(16, 2);
-  Serial.begin(speed); // Configura la velocidad de transmisión inicial (modificar según las pruebas)
+  Serial.begin(speed);  // Configura la velocidad de transmisión inicial (modificar según las pruebas)
   mySerial.begin(speed);
-  randomSeed(analogRead(0));
-  for (int i = 0; i < payloadSize; i++)
-  {
-    data[i] = random(256);
-  }
+  randomSeed(1);
 }
 
-void loop()
-{
-  uint8_t *frameA = nullptr;
-  uint8_t *frameB = nullptr;
-  for (int i = 0; i < numberOfTests; i++)
-  {
-    // Start test!
+void loop() {
+  uint8_t *frameA;
+  uint8_t *frameB;
+  uint8_t NS = random(256);
+  for (int i = 0; i < payloadSize; i++) {
+    int num = random(1, 256);
+    if (num == 126) {
+      num++;
+    }
+    data[i] = num;
+  }
+  for (int i = 0; i < numberOfTests; i++) {
     int bytesCopied = 0;
     int copySize = 200;
 
-    while (bytesCopied < payloadSize)
-    {
+    frameA = buildFrameA(initialHandshake, (uint16_t)speed, (uint16_t)payloadSize);
+    sendFrame(frameA, 0, -1);
+    while (bytesCopied < payloadSize) {
+      if (!first_transmission) {
+        frameA = buildFrameA(nonInitialHandshake, (uint16_t)speed, (uint16_t)payloadSize);
+        sendFrame(frameA, 0, -1);
+      }
+      for (int i = 0; i < payloadSize; i++) {
+        int num = random(1, 256);
+        if (num == 126) {
+          num++;
+        }
+        data[i] = num;
+      }
       uint8_t NS = random(256);
-      // Envío primera comunicación
-      if (first_transmission)
-      {
-        // Serial.println("First A transmission");
-        frameA = buildFrameA(initialHandshake, (uint16_t)speed, (uint16_t)payloadSize);
-        Serial.print(frameA[0], BIN);
-        sendFrame(frameA, NS, -1);
-        first_transmission = false;
-      }
-      // Envío comunicación no inicial
-      else
-      {
-        // Serial.println("Non initial A transmission");
-        frameA = buildFrameA(nonnitialHandshake, (uint16_t)speed, (uint16_t)payloadSize);
-        sendFrame(frameA, NS, -1);
-      }
 
-      int remainingBytes = payloadSize - bytesCopied;
-      if (remainingBytes < copySize)
-      {
-        copySize = remainingBytes;
-      }
-
-      bytesCopied += copySize;
-
-      // Envío trama B
-      // Serial.println("\nB transmission");
       frameB = buildFrameB(NS, infoFrame, data + bytesCopied);
       sendFrame(frameB, NS, 0);
-      // Envío trama A (Faltan más comunicaciones en la prueba)
-      // Serial.println("\nlast comm for this test A transmission");
+
+      bytesCopied += payloadSize - copySize;
+
       frameA = buildFrameA(endCurrentComm, (uint16_t)speed, (uint16_t)payloadSize);
-      sendFrame(frameA, NS, -1);
-      // Serial.print("\n");
+      sendFrame(frameA, 0, -1);
+
+      first_transmission = false;
     }
-    // Serial.println("This test is complete");
-    first_transmission = true;
+    frameA = buildFrameA(finalComm, (uint16_t)speed, (uint16_t)payloadSize);
+    sendFrame(frameA, 0, -1);
   }
-  // Envío trama A (Fin total de la comunicación)
-  // Serial.print("\n");
-  // Serial.println("Last A transmission");
-  frameA = buildFrameA(finalComm, (uint16_t)speed, (uint16_t)payloadSize);
-  sendFrame(frameA, 0, -1);
-  // Serial.println("\nDone testing");
-  while (true)
-  {
+  //frameA = buildFrameA(finalComm, (uint16_t)speed, (uint16_t)payloadSize);
+  //sendFrame(frameA, 0, -1);
+  // for (int i = 0; i < numberOfTests; i++) {
+  //   // Start test!
+  //   int bytesCopied = 0;
+  //   int copySize = 200;
+  //   // Envío primera comunicación
+  //   if (first_transmission) {
+  //     // Serial.println("First A transmission");
+  //     frameA = buildFrameA(initialHandshake, (uint16_t)speed, (uint16_t)payloadSize);
+  //     sendFrame(frameA, 0, -1);
+  //   }
+  //   while (bytesCopied < payloadSize) {
+  //     for (int i = 0; i < payloadSize; i++) {
+  //       int num = random(1, 256);
+  //       if (num == 126) {
+  //         num++;
+  //       }
+  //       data[i] = num;
+  //     }
+  //     uint8_t NS = random(256);
+  //     // Envío comunicación no inicial
+  //     if (!first_transmission) {
+  //       // Serial.println("Non initial A transmission");
+  //       frameA = buildFrameA(nonnitialHandshake, (uint16_t)speed, (uint16_t)payloadSize);
+  //       sendFrame(frameA, 0, -1);
+  //     }
+  //     first_transmission = false;
+  //     int remainingBytes = payloadSize - bytesCopied;
+  //     if (remainingBytes < copySize) {
+  //       copySize = remainingBytes;
+  //     }
+
+  //     bytesCopied += copySize;
+  //     Serial.print(bytesCopied);
+  //     // Envío trama B
+  //     // Serial.println("\nB transmission");
+  //     frameB = buildFrameB(NS, infoFrame, data + bytesCopied);
+  //     sendFrame(frameB, NS, 0);
+  //     // Envío trama A (Faltan más comunicaciones en la prueba)
+  //     // Serial.println("\nlast comm for this test A transmission");
+  //     frameA = buildFrameA(endCurrentComm, (uint16_t)speed, (uint16_t)payloadSize);
+  //     sendFrame(frameA, 0, -1);
+  //     // Serial.print("\n");
+  //   }
+  //   // Serial.println("This test is complete");
+  //   first_transmission = true;
+  // }
+  // // Envío trama A (Fin total de la comunicación)
+  // // Serial.print("\n");
+  // // Serial.println("Last A transmission");
+  // frameA = buildFrameA(finalComm, (uint16_t)speed, (uint16_t)payloadSize);
+  // sendFrame(frameA, 0, -1);
+  // // Serial.println("\nDone testing");
+  while (true) {
   }
 }
